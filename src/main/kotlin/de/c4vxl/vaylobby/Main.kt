97c0572aa0de793753c86957b1de0c46d5b7575e
c4vxl <manage@c4vxl.de>
2026-04-25 16:39:44 +0200

    Initial Commit

---
+++ src/main/kotlin/de/c4vxl/vaylobby/Main.kt
@@ -0,0 +1,50 @@
+package de.c4vxl.vaylobby
+
+import de.c4vxl.vaycoreapi.language.Lang
+import de.c4vxl.vaycoreapi.utils.ResourceUtils
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIPaperConfig
+import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Logger
+
+class Main : JavaPlugin() {
+    companion object {
+        lateinit var instance: Main
+        lateinit var logger: Logger
+    }
+
+    override fun onLoad() {
+        instance = this
+        Main.logger = this.logger
+
+        // Load CommandAPI
+        CommandAPI.onLoad(
+            CommandAPIPaperConfig(this)
+                .silentLogs(true)
+                .verboseOutput(false)
+        )
+    }
+
+    override fun onEnable() {
+        // Enable CommandAPI
+        CommandAPI.onEnable()
+
+        // Register translations
+        ResourceUtils.readResource("langs", Main::class.java).split("\n")
+            .forEach { langName ->
+                Lang.provideLanguageTranslations<Main>(
+                    langName,
+                    ResourceUtils.readResource("lang/$langName.yml", Main::class.java)
+                )
+            }
+
+        logger.info("[+] $name has been enabled!")
+    }
+
+    override fun onDisable() {
+        // Disable CommandAPI
+        CommandAPI.onDisable()
+
+        logger.info("[+] $name has been disabled!")
+    }
+}
